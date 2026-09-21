f1877528a644d865ded23686f59669ab29f2514f
VictorTelless <victinhotelles@gmail.com>
2026-09-20 22:13:28 -0300

    Configura dominio do Render

--- ecomply/settings.py
+++ ecomply/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-)*)*o689fxe%9_kss=sk6s7(!994a9ehlun_ubex34j=ozd9gx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ecomply-5taq.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
